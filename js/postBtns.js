5462972200b01907c7b8b3c0b51ba4ea3f98a28e
AlbertK12 <125406140+AlbertK12@users.noreply.github.com>
2024-10-25 10:54:51 -0600

    Cambios para el menu

--- js/postBtns.js
+++ js/postBtns.js
@@ -1,39 +1,4 @@
 window.addEventListener("load", function(event) {
-    //Cerrar sesion
-    const cerrarSesionBtn = document.getElementById('cerrarSesion');
-    cerrarSesionBtn.addEventListener('click', function(){
-        location.replace("login.html");
-    });
-    //Cerrar sesion
-
-    //Ir a home
-    const homeBtn = document.getElementById('home');
-    homeBtn.addEventListener('click', function(){
-        console.log("Fui a home");
-    });
-    //Ir a home
-
-    //Ir a etiquetas
-    const etiquetasBtn = document.getElementById('etiquetas');
-    etiquetasBtn.addEventListener('click', function(){
-        console.log("Fui a etiquetas");
-    });
-    //Ir a etiquetas
-
-    //Ir a React
-    const reactBtn = document.getElementById('react');
-    reactBtn.addEventListener('click', function(){
-        console.log("Fui a react");
-    });
-    //Ir a React
-
-    //Ir a js
-    const jsBtn = document.getElementById('js');
-    jsBtn.addEventListener('click', function(){
-        console.log("Fui a js");
-    });
-    //Ir a js
-
     //like
     const likeBtn = document.getElementById('like');
     likeBtn.addEventListener('click', function(){
